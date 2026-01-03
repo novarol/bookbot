d2ba6f68380d275b4d46ae8d14e247fff27f6f04
novarol <novarol3d@gmail.com>
2026-01-03 22:24:50 +0100

    feat: read book path from cli argument

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import sys
+from stats import count_words_in_book, count_characters_in_book, get_sorted_character_count_list
+
 def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
-from stats import count_words_in_book, count_characters_in_book, get_sorted_character_count_list
-
 def main():
-  path_to_book = 'books/frankenstein.txt'
+  if len(sys.argv) != 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
+  path_to_book = sys.argv[1]
   book_text = get_book_text(path_to_book)
   word_count = count_words_in_book(book_text)
   character_count = count_characters_in_book(book_text)
