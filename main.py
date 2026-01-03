a5a6aa7edac86fb8821baf781d8cef40aa9e12d0
novarol <novarol3d@gmail.com>
2026-01-03 21:45:07 +0100

    feat: implement count characters in book

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
-from stats import count_words_in_book
+from stats import count_words_in_book, count_characters_in_book
 
 def main():
   book_text = get_book_text('books/frankenstein.txt')
   word_count = count_words_in_book(book_text)
+  character_count = count_characters_in_book(book_text)
+  
   print(f'Found {word_count} total words')
+  print(character_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def count_words_in_book(book_text):
   return len(book_text.split())
+
+def count_characters_in_book(book_text):
+  character_count = {}
+  for char in book_text.lower():
+    if char not in character_count:
+      character_count[char] = 0
+    character_count[char] += 1
+
+  return character_count
