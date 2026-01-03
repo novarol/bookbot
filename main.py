ba26d3bcb5a635c74b2f4d3d1b6b32d2527237ab
novarol <novarol3d@gmail.com>
2026-01-03 22:13:04 +0100

    feat: add get sorted character count list, add report print format

--- main.py
+++ main.py
@@ -2,14 +2,29 @@ def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
-from stats import count_words_in_book, count_characters_in_book
+from stats import count_words_in_book, count_characters_in_book, get_sorted_character_count_list
 
 def main():
-  book_text = get_book_text('books/frankenstein.txt')
+  path_to_book = 'books/frankenstein.txt'
+  book_text = get_book_text(path_to_book)
   word_count = count_words_in_book(book_text)
   character_count = count_characters_in_book(book_text)
+  sorted_character_count = get_sorted_character_count_list(character_count)
+
+  print('============ BOOKBOT ============')
+  print(f'Analyzing book found at {path_to_book}...')
   
+  print('----------- Word Count ----------')
   print(f'Found {word_count} total words')
-  print(character_count)
+
+  print('--------- Character Count -------')
+  for item in sorted_character_count:
+    char = item['char']
+    if not char.isalpha():
+      continue
+
+    print(f'{char}: {item['num']}')
+
+  print('============= END ===============')
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,16 @@ def count_characters_in_book(book_text):
     character_count[char] += 1
 
   return character_count
+
+def sort_on_num(dict):
+  return dict['num']
+
+def get_sorted_character_count_list(character_count_dict):
+  character_count_list = []
+
+  for char in character_count_dict:
+    character_count_list.append({ 'char': char, 'num': character_count_dict[char] })
+
+  character_count_list.sort(reverse=True, key=sort_on_num)
+
+  return character_count_list
