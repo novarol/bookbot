bb307eb9e1df77588d05ec2ab193c303e99c546b
novarol <novarol3d@gmail.com>
2026-01-03 20:43:45 +0100

    ref: add stats module, move count words in book

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
-def count_words_in_book(book_text):
-  return len(book_text.split())
+from stats import count_words_in_book
 
 def main():
   book_text = get_book_text('books/frankenstein.txt')
