566f866321c961b65d0ebd10cb3e739774fa5040
novarol <novarol3d@gmail.com>
2026-01-03 20:40:46 +0100

    feat: implement count words in book

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
 
+def count_words_in_book(book_text):
+  return len(book_text.split())
+
 def main():
-  print(get_book_text('books/frankenstein.txt'))
+  book_text = get_book_text('books/frankenstein.txt')
+  word_count = count_words_in_book(book_text)
+  print(f'Found {word_count} total words')
 
 main()
